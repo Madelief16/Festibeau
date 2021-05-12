d16085a9a40a512b382393fe91c8be67a9434129
beau rikkoert <110368@st-maartenscollege.nl>
2021-05-12 09:39:05 +0200

    goede stukje geplakt van gegevens ophalen MySQL

--- Festibeau/Controllers/HomeController.cs
+++ Festibeau/Controllers/HomeController.cs
@@ -22,126 +22,56 @@ namespace Festibeau.Controllers
         {
             return View();
         }
+        public List<string> GetNames()
         {
-  public class HomeController : Controller
-        {
-            private readonly ILogger<HomeController> _logger;
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=0P%Y9fI2GdO#;";
 
-            public HomeController(ILogger<HomeController> logger)
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<string> names = new List<string>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
-                _logger = logger;
-            }
+                // verbinding openen
+                conn.Open();
 
-            public IActionResult Index()
-            {
-                // alle namen ophalen
-                var products = GetProducts();
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
 
-                // stop de namen in de html
-                return View(products);
-            }
-
-            public IActionResult Privacy()
-            {
-                return View();
-            }
-
-            public List<Product> GetProducts()
-            {
-                // stel in waar de database gevonden kan worden
-                string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110368;Uid=110368;Pwd=inf2021SQL;";
-
-                // maak een lege lijst waar we de namen in gaan opslaan
-                List<Product> products = new List<Product>();
-
-                // verbinding maken met de database
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
                 {
-                    // verbinding openen
-                    conn.Open();
-
-                    // SQL query die we willen uitvoeren
-                    MySqlCommand cmd = new MySqlCommand("select * from product", conn);
-
-                    // resultaat van de query lezen
-                    using (var reader = cmd.ExecuteReader())
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
                     {
-                        // elke keer een regel (of eigenlijk: database rij) lezen
-                        while (reader.Read())
-                        {
-                            Product p = new Product
-                            {
-                                // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
-                                Id = Convert.ToInt32(reader["Id"]),
-                                Beschikbaarheid = Convert.ToInt32(reader["Beschikbaarheid"]),
-                                Naam = reader["Naam"].ToString(),
-                                Prijs = reader["Prijs"].ToString(),
-                            };
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
 
-                            // voeg de naam toe aan de lijst met namen
-                            products.Add(p);
-                        }
+                        // voeg de naam toe aan de lijst met namen
+                        names.Add(Name);
                     }
                 }
-
-                // return de lijst met namen
-                return products;
             }
 
-            public List<string> GetNames()
-            {
-                // stel in waar de database gevonden kan worden
-                string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=0P%Y9fI2GdO#;";
-
-                // maak een lege lijst waar we de namen in gaan opslaan
-                List<string> names = new List<string>();
-
-                // verbinding maken met de database
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                {
-                    // verbinding openen
-                    conn.Open();
-
-                    // SQL query die we willen uitvoeren
-                    MySqlCommand cmd = new MySqlCommand("select * from product", conn);
-
-                    // resultaat van de query lezen
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        // elke keer een regel (of eigenlijk: database rij) lezen
-                        while (reader.Read())
-                        {
-                            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
-                            string Name = reader["Naam"].ToString();
-
-                            // voeg de naam toe aan de lijst met namen
-                            names.Add(Name);
-                        }
-                    }
-                }
-
-                // return de lijst met namen
-                return names;
-            }
-
-            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-            public IActionResult Error()
-            {
-                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-            }
+            // return de lijst met namen
+            return names;
         }
-    }
-    public IActionResult Privacy => View();
 
-    IActionResult View()
-    {
-        throw new NotImplementedException();
-    }
-
-    [Route("contact")]
-        public IActionResult contact()
+        [Route("Privacy")]
+        public IActionResult Festivals()
         {
             return View();
+        }
+
+        [Route("contact")]
+    public IActionResult contact(string voornaam, string achternaam)
+    {
+        
+        ViewData["voornaam"] = voornaam;
+        ViewData["achternaam"] = achternaam;
+        
+        return View();
         }
 
         [Route("Festivals")]
