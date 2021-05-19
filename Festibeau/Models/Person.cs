d4a2e42c1aba4aaf1a9de24d7f6a4da1d673d330
Madelief van der Bles <110461@st-maartenscollege.nl>
2021-05-19 09:52:38 +0200

    INhoud person

--- Festibeau/Models/Person.cs
+++ Festibeau/Models/Person.cs
@@ -1,1 +1,12 @@
+namespace SendAndStore.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
 
+    }
