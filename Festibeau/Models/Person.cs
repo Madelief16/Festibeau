d22479f9afe7f4ef50d0890ecd534a65979e45ea
Madelief van der Bles <110461@st-maartenscollege.nl>
2021-05-19 10:53:31 +0200

    Required bij person aangepast

--- Festibeau/Models/Person.cs
+++ Festibeau/Models/Person.cs
@@ -4,13 +4,13 @@ namespace SendAndStore.Models
 {
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "Verplicht in te vullen")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Verplicht in te vullen")]
         public string LastName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Verplicht in te vullen")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Verplicht in te vullen")]
         public string Phone { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
