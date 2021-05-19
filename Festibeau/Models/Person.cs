ce7c455481ade61a5fe46f71f39d00d135976a88
Madelief van der Bles <110461@st-maartenscollege.nl>
2021-05-19 10:16:59 +0200

    required bij person

--- Festibeau/Models/Person.cs
+++ Festibeau/Models/Person.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SendAndStore.Models
 {
     public class Person
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Phone { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
